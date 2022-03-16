7736c8d308dec939aba9a56303e069a3949960c4
nitro <77024697+nitro-n7@users.noreply.github.com>
2022-03-16 09:40:15 +0100

    Update answers.py

--- answers.py
+++ answers.py
@@ -11,11 +11,6 @@ for index, slide in enumerate(json):
     for i in range(len(slide.get("choices"))):
         
         if slide["choices"][i]["correct"]:
-            
-            if len(slide.get("choices")) == 2:
-                coulours_list = coulours[:2][::-1]
-                
-            else:
-                coulours_list = colours
-            
+            colours_list = colours[:2][::-1] if len(slide.get("choices")) == 2 else colours
+
             print(f"{index+1}:\n{slide['choices'][i].get('answer')}\n{colours_list[i]}\n")
