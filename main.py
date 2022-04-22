7904cbc0d2532eb0c60e0c6feaf4b9784dae26ed
nitro <77024697+nitro-n7@users.noreply.github.com>
2022-04-22 18:18:51 +0200

    Fixed Bug + Improved Readability

--- main.py
+++ main.py
@@ -4,42 +4,44 @@ from threading import Thread
 from kahoot import client
 
 
-def answers(id):  
-    # Get Kahoot Quiz Answers
-    url = "https://play.kahoot.it/rest/kahoots/"+id
+def get_answers(id):  
+    url = f"https://play.kahoot.it/rest/kahoots/{id}"
 
-    colors = ["red", "blue", "yellow", "green"]
+    color_list = ["red", "blue", "yellow", "green"]
     json = j.loads(ur.urlopen(url).read())["questions"]
 
     for index, slide in enumerate(json):
         for i in range(len(slide.get("choices"))):
-            
             if slide["choices"][i]["correct"]:
-                colors_list = colors[:2][::-1] if len(slide.get("choices")) == 2 else colors
+                if len(slide.get("choices")) == 2:
+                    color = color_list[:2][::-1]
+                else:
+                    color = color_list
 
-                print(f"{index+1}:\n{slide['choices'][i].get('answer')}\n{colours_list[i]}\n")
+                print(f"{index+1}:\n{slide['choices'][i].get('answer')}\n{color_list[i]}\n")
         
         
-def create_bot(id, name):
-    # Join Kahoot Lobby w/ Bot
+def join_game(id, name):
     bot = client()
     bot.join(id, name)
 
     
 def main():
     option = int(input("\n[1] ANSWERS\n[2] FLOODER\n"))
-
+    
     if option == 1:
+        quiz_id = input("[-] QUIZ ID: ")
+        get_answers(quiz_id)
+        
+    elif option == 2:
         bot_count = int(input("[-] NUMBER OF BOTS: "))
         game_id = int(input("[-] GAME PIN: "))
         for i in range(bot_count): 
-            Thread(target=create_bot(game_id, str(i))).start()
+            Thread(target=join_game(game_id, str(i))).start()
 
-    elif option == 2:
-        quiz_id = input("[-] QUIZ ID: ")
-        answers(quiz_id)
-
-    else: main()
+    else:
+        print("Invalid Option")
+        main()
     
 
 if __name__=="__main__":
