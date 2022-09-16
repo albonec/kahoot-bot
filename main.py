d0a1276b55115df0e7f12a70e367e447943b0029
nitro <77024697+nitro-n7@users.noreply.github.com>
2022-09-16 13:41:24 +0000

    Removed 'botter'
    KahootPY is outdated and no longer in use.

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import urllib.request as ur
 import json as j
-from threading import Thread
-from kahoot import client
-
 
 def get_answers(id):  
     url = "https://play.kahoot.it/rest/kahoots/"+id
@@ -15,24 +12,6 @@ def get_answers(id):
             if slide["choices"][i]["correct"]:
                 print(f"{index+1}:\n{slide['choices'][i].get('answer')}\n{color_list[i]}\n")
         
-        
-def create_bot(id, name):
-    bot = client()
-    bot.join(id, name)
-
-        
 while True:
-    option = int(input("\n[1] ANSWERS\n[2] FLOODER\n~ "))
-    
-    if option == 1:
-        quiz_id = input("[-] QUIZ ID: ")
-        get_answers(quiz_id)
-        
-    elif option == 2:
-        bot_count = int(input("[-] NUMBER OF BOTS: "))
-        game_id = int(input("[-] GAME PIN: "))
-        for i in range(bot_count): 
-            Thread(target=create_bot(game_id, str(i))).start()
-
-    else:
-        print("Invalid Option")
+        get_answers(input("[-] QUIZ ID: "))
+       
