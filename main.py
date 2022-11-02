9edd7fe2bd099ca96bde77cdbafa6ee17dcd912b
albonec <agbonecchi@gmail.com>
2022-11-01 22:00:16 -0700

    screencapture stuff

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-import urllib.request as ur
-import json as j
+from getAnswers import get_answers
 
-def get_answers(id):  
-    url = "https://play.kahoot.it/rest/kahoots/"+id
-
-    color_list = ["red", "blue", "yellow", "green"]
-    json = j.loads(ur.urlopen(url).read())["questions"]
-
-    for index, slide in enumerate(json):
-        for i in range(len(slide.get("choices"))):
-            if slide["choices"][i]["correct"]:
-                print(f"{index+1}:\n{slide['choices'][i].get('answer')}\n{color_list[i]}\n")
-        
 while True:
         get_answers(input("[-] QUIZ ID: "))
-       
