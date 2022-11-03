e06ae95ca9da27aa0a841e2f2ad66d87fc379e31
albonec <agbonecchi@gmail.com>
2022-11-02 21:30:38 -0700

    color/contours now working

--- detectColorPosition.py
+++ detectColorPosition.py
@@ -1,16 +1,17 @@
 import numpy as np
 import cv2
 from mss import mss
-from PIL import Image
-
-bounding_box = {'top': 100, 'left': 0, 'width': 400, 'height': 300}
 
 sct = mss()
+bounding_box = {'top': 0, 'left': 0, 'width': 1920, 'height': 1080}
+
+def detect_color(hMin, hMax, sMin, sMax, vMin, vMax):
+    img = np.array(sct.grab(bounding_box))
+    imgFiltered = cv2.bitwise_and(img, img, mask=cv2.inRange(cv2.cvtColor(img, cv2.COLOR_BGR2HSV), np.array([hMin, sMin, vMin]), np.array([hMax, sMax, vMax])))
+    imgCanny = cv2.Canny(imgFiltered, 50, 50)
+    cv2.imshow("canny", imgCanny)
+    cv2.waitKey(1)
+
 
 while True:
-    sct_img = sct.grab(bounding_box)
-    cv2.imshow('screen', np.array(sct_img))
-
-    if (cv2.waitKey(1) & 0xFF) == ord('q'):
-        cv2.destroyAllWindows()
-        break
+    detect_color(0, 0, 90, 110, 36, 56)
